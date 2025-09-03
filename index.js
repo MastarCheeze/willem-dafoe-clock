18b874f3b8618b28bcf5a32bec6429400b9ec2c3
MastarCheeze <mon10180218@gmail.com>
2025-09-03 23:55:00 +0800

    refactor code

--- index.js
+++ index.js
@@ -1,84 +1,103 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+/* Get rotation of long and short clock hand */
+function getRotation(date) {
+  const minInSecs = date.getMinutes() * 60 + date.getSeconds();
+  const hourInSecs = date.getHours() * 3600 + minInSecs;
+
+  return [
+    (minInSecs / 3600) * 2 * Math.PI,
+    (hourInSecs / (12 * 3600)) * 2 * Math.PI,
+  ];
+}
+
+/* Create images */
+const imgSources = {
+  background: "assets/background.png",
+  clockFace: "assets/clock-face.png",
+  body: "assets/dafoe-body.png",
+  longLeg: "assets/dafoe-long-leg.png",
+  shortLeg: "assets/dafoe-short-leg.png",
+};
+const img = {};
+for (const key in imgSources) {
+  img[key] = new Image();
+  img[key].src = imgSources[key];
+}
+
+const baseScalePx = [1200, 1200]; // width and height of max sized image element
+let mainScale; // scale set by resizeCanvas based on canvas dimensions
+const clockScale = 0.9; // size multiplier of the clock
+
+/* Draw function that calls every draw tick */
+function draw() {
+  ctx.clearRect(
+    -canvas.width / 2,
+    -canvas.height / 2,
+    canvas.width / 2,
+    canvas.height / 2,
+  );
+  ctx.save();
+  ctx.save();
+
+  drawInCentre(img.background);
+  drawInCentre(img.clockFace, clockScale);
+
+  const [minRot, hourRot] = getRotation(new Date());
+
+  ctx.rotate(minRot);
+  drawInCentre(img.longLeg, clockScale);
+  ctx.restore();
+
+  ctx.rotate(hourRot);
+  drawInCentre(img.shortLeg, clockScale);
+  ctx.restore();
+
+  drawInCentre(img.body, clockScale);
+}
+
+// draw image centered on the canvas
+function drawInCentre(img, scale = 1) {
+  ctx.drawImage(
+    img,
+    (-img.width * mainScale * scale) / 2,
+    (-img.height * mainScale * scale) / 2,
+    img.width * mainScale * scale,
+    img.height * mainScale * scale,
+  );
+}
+
+/* Resize canvas and set new scale to fit screen */
 function resizeCanvas() {
   canvas.width = canvas.parentElement.clientWidth;
   canvas.height = canvas.parentElement.clientHeight;
 
-  scale = Math.min(
-    canvas.height / clockFaceImg.height,
-    canvas.width / clockFaceImg.width,
+  mainScale = Math.min(
+    canvas.width / baseScalePx[0],
+    canvas.height / baseScalePx[1],
   );
-}
 
-const clockFaceImg = new Image();
-clockFaceImg.src = "assets/clock-face.png";
-const bodyImg = new Image();
-bodyImg.src = "assets/dafoe-body.png";
-const longLegImg = new Image();
-longLegImg.src = "assets/dafoe-long-leg.png";
-const shortLegImg = new Image();
-shortLegImg.src = "assets/dafoe-short-leg.png";
-let scale = 1;
-
-function draw() {
+  // move context to centre of canvas
   ctx.reset();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.translate(canvas.width / 2, canvas.height / 2);
-  ctx.save();
-  ctx.save();
-
-  ctx.drawImage(
-    clockFaceImg,
-    (-clockFaceImg.width * scale) / 2,
-    (-clockFaceImg.height * scale) / 2,
-    clockFaceImg.width * scale,
-    clockFaceImg.height * scale,
-  );
-
-  const [minRot, hourRot] = getRot(new Date());
-
-  ctx.rotate(minRot);
-  ctx.drawImage(
-    longLegImg,
-    (-longLegImg.width * scale) / 2,
-    (-longLegImg.height * scale) / 2,
-    longLegImg.width * scale,
-    longLegImg.height * scale,
-  );
-  ctx.restore();
-
-  ctx.rotate(hourRot);
-  ctx.drawImage(
-    shortLegImg,
-    (-shortLegImg.width * scale) / 2,
-    (-shortLegImg.height * scale) / 2,
-    shortLegImg.width * scale,
-    shortLegImg.height * scale,
-  );
-  ctx.restore();
-
-  ctx.drawImage(
-    bodyImg,
-    (-bodyImg.width * scale) / 2,
-    (-bodyImg.height * scale) / 2,
-    bodyImg.width * scale,
-    bodyImg.height * scale,
-  );
 }
 
-function getRot(date) {
-  const minSecs = date.getMinutes() * 60 + date.getSeconds();
-  const hourSecs = date.getHours() * 3600 + minSecs;
+/* Wait for all images to load */
+const imagesToLoad = Object.values(img);
+Promise.all(
+  imagesToLoad.map(
+    (img) =>
+      new Promise((resolve) => {
+        img.onload = resolve;
+      }),
+  ),
+).then(() => {
+  resizeCanvas();
+  window.addEventListener("resize", () => {
+    resizeCanvas();
+    draw();
+  });
 
-  return [
-    (minSecs / 3600) * 2 * Math.PI,
-    (hourSecs / (12 * 3600)) * 2 * Math.PI,
-  ];
-}
-
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
-window.addEventListener("resize", draw);
-
-setInterval(draw, 100);
+  setInterval(draw, 200);
+});
